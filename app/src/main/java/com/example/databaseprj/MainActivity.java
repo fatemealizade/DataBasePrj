35424453e2718487e83b5115f46c36820d970fe3
Fateme <53145698+fatemealizade@users.noreply.github.com>
2019-08-25 17:29:39 +0430

    Update MainActivity.java

--- app/src/main/java/com/example/databaseprj/MainActivity.java
+++ app/src/main/java/com/example/databaseprj/MainActivity.java
@@ -10,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        DataBaseHandler dataBaseHandler = new DataBaseHandler(MainActivity.this);
+
+        //create contact obj first
+        Contact jeremy = new Contact();
+        jeremy.setName("Jeremy");
+        jeremy.setPhoneNum("0809343849");
+
+        dataBaseHandler.addContact(jeremy);
+        List<Contact> contactList = dataBaseHandler.getAllContacts();
+
+        for (Contact contact : contactList) {
+            Log.d("Main", "onCreate: " + contact.getId());
+        }
     }
 }
